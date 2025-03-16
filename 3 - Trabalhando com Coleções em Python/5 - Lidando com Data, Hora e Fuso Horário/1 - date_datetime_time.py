e0740ab7db98b8d54943d01f7f7c0231d849e41c
IsaacReis <isaacadriano96@gmail.com>
2025-03-16 13:22:23 -0300

    Adicionando parte de Data e Hora.

---
+++ 3 - Trabalhando com Coleções em Python/5 - Lidando com Data, Hora e Fuso Horário/1 - date_datetime_time.py
@@ -0,0 +1,12 @@
+from datetime import date, datetime, time
+
+date = date(2023, 7, 19)
+print(date)
+print(date.today())
+
+data_hora = datetime(2023, 7, 19, 10, 30, 20)
+print(data_hora)
+print(datetime.today())
+
+hora = time(10, 20, 0)
+print(hora)
